feb7a31e849f0330441546c6764ea15d90c2b8d5
Matthew Witman <mnwranger@hotmail.com>
2017-01-09 01:52:25 -0500

    fixed a few issues with the addon

--- lua/weapons/regen.lua
+++ lua/weapons/regen.lua
@@ -1,12 +1,15 @@
 AddCSLuaFile()
-resource.AddFile("vgui/ttt/sirlag_regeneration.png")
+resource.AddFile("vgui/ttt/sirlag_regeneration.vtf")
 
+ENABLE_SOUND = true		-- Toggles a sound to play each regen tick.
 EQUIP_REGEN = 5984 		-- Make sure this is unique. Leaving it as is should be fine in almost all scenarios.
 HEATH_TO_REGEN = 50		-- How much health to regenerate.
-TIME_FOR_REGEN = 30		-- How Long (in seconds) it should take to regenerate the heath.
+TIME_FOR_REGEN = 20		-- How Long (in seconds) it should take to regenerate the heath.
 REGEN_CHUNKS = 10		-- How many ticks it takes to regenerate the health. This can be set to 1, which would
 						-- regenerate all of the health after your TIME_FOR_REGEN is over.
 						-- Setting it to 0 will throw major script errors. just don't do it.
+
+
 
 --This is a hook to set up the addon in the traitor shop. There is also a line you can uncomment to add it to detective 
 hook.Add( "InitPostEntity", "AddRegenToStore", function() 
@@ -14,9 +17,9 @@ hook.Add( "InitPostEntity", "AddRegenToStore", function()
               id = EQUIP_REGEN,
               loadout = false,
               type = "item_passive",
-              material = "vgui/ttt/sirlag_regeneration.png",
+              material = "vgui/ttt/sirlag_regeneration.vtf",
               name = "Heath Regen",
-              desc = "Passively Restores "..HEATH_TO_REGEN.."\n"
+              desc = "Passively Restores "..HEATH_TO_REGEN.." health over "..TIME_FOR_REGEN.." seconds\n"
       }
       table.insert( EquipmentItems[ROLE_TRAITOR], Regen )
 	  --Uncomment the next line to add the item to the detective shop as well.
@@ -26,12 +29,13 @@ end)
 --Hooks buying the regen item from the traitor shop. After grabbing that, it starts the healing process.
 hook.Add("TTTOrderedEquipment", "OrderedARegenItem", function(ply)
 	if ply:HasEquipmentItem(EQUIP_REGEN) then
-		if(ply.hasregen == false) then
+		if(ply.hasregen ~= true) then
 			ply.hasregen = true
 			timer.Create( "HeathRegenerationTimer", math.floor(TIME_FOR_REGEN / REGEN_CHUNKS), REGEN_CHUNKS, function()
 				tempHealth = ply:Health() + math.floor(HEATH_TO_REGEN / REGEN_CHUNKS)
 				if (tempHealth > ply:GetMaxHealth()) then	tempHealth = ply:GetMaxHealth()	end
 				ply:SetHealth(tempHealth)
+				if (ENABLE_SOUND)	then surface.PlaySound("buttons/blip1.wav")	end
 			end)
 		end
 	end 
